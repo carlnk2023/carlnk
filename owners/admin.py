bea0975fcad3589f1db38dfb7c0344ad18903401
carlnk2023 <127796179+carlnk2023@users.noreply.github.com>
2023-03-15 01:10:59 +0200

    Delete owners directory

--- owners/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Owner
-# Register your models here.
-
-class OwnerAdmin(admin.ModelAdmin):
-	list_display = ('user', 'business_name', 'date_created')
-	search_fields = ('business_name', 'last_name', 'phone_number', 'whatsapp_number')
-
-admin.site.register(Owner, OwnerAdmin)
