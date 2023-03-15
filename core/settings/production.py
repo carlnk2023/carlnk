2d37695085d772db81cdc99c483d77bebe1f784f
carlnk2023 <carlnk2023@gmail.com>
2023-03-15 02:53:36 +0200

    Static dirs settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -27,7 +27,10 @@ CSRF_TRUSTED_ORIGINS = ['https://carlnk.co']
 # Media and Staticfiles
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
